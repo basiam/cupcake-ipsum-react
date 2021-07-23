d927cf02a8e56482ca1cf0426aed723da67695f1
Basia Madej <basia@lunarlogic.io>
2021-07-23 15:22:35 +0200

    html for form and result

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import './App.css';
+import { Form } from './Form';
+import { Result } from './Result';
 
 function App() {
   return (
     <div className="App">
-      <form>
-        <label for="number_of_paragraphs">
-          <input id="number_of_paragraphs" name="numberOfParagraphs" />Paragraphs
-        </label>
-        <div id="length_of_paragraph">
-          <label><input name="lengthOfParagraph" type="radio" value="long" />Long</label>
-          <label><input name="lengthOfParagraph" type="radio" value="medium" />Medium</label>
-          <label><input name="lengthOfParagraph" type="radio" value="short" />Short</label>
-        </div>
-        <label>
-          <input name="startsWithCupcakeIpsum" type="checkbox" />
-          Start with<br/>
-          <span>"Cupcake ipsum dolor sit amet"</span>
-        </label>
-        <label>
-          <input name="containsLove" type="checkbox" />
-          "Give your text some "love"
-        </label>
-        <div class="button-with-shadow generate">
-          <button id="generate_button">Generate</button>
-          <div class="shadow"></div>
-        </div>
-      </form>
+      <Form />
+      <Result />
     </div>
   );
 }
---
+++ src/Result.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const text = "dadasdas";
+
+export function Result() {
+
+  return (
+    <div id="cupcake_ipsum">
+      <div className="bg-top">
+
+      </div>
+      <div className="body">
+        <p className="paragraph">
+          ${text}
+        </p>
+      </div>
+      <div className="bg-bottom">
+
+      </div>
+      <div className="text-controls">
+        <div className="button-with-shadow clipboard">
+          <button id="copy_button">
+              Copy to clipboard
+          </button>
+          <div className="shadow">
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
