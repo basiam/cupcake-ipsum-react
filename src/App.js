0b3d335485a5053388c19b3dd90a802f5e090f8c
Basia Madej <basia@lunarlogic.io>
2021-07-23 12:28:33 +0200

    Static files and styles, just copied

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src=src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label for="number_of_paragraphs">
+          <input id="number_of_paragraphs" name="numberOfParagraphs" />Paragraphs
+        </label>
+        <div id="length_of_paragraph">
+          <label><input name="lengthOfParagraph" type="radio" value="long" />Long</label>
+          <label><input name="lengthOfParagraph" type="radio" value="medium" />Medium</label>
+          <label><input name="lengthOfParagraph" type="radio" value="short" />Short</label>
+        </div>
+        <label>
+          <input name="startsWithCupcakeIpsum" type="checkbox" />
+          Start with<br/>
+          <span>"Cupcake ipsum dolor sit amet"</span>
+        </label>
+        <label>
+          <input name="containsLove" type="checkbox" />
+          "Give your text some "love"
+        </label>
+        <div class="button-with-shadow generate">
+          <button id="generate_button">Generate</button>
+          <div class="shadow"></div>
+        </div>
+      </form>
     </div>
   );
 }
