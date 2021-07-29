0c29a9889e8c2fc21990cfa06db324af0aeb3aae
Basia Madej <basia@lunarlogic.io>
2021-07-29 16:18:22 +0200

    Fix coping

--- src/Result.js
+++ src/Result.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 export function Result({ paragraphs }) {
 
+  const fallback = textToCopy => {
+    const textArea = document.createElement("textarea");
+    textArea.value = textToCopy;
+    textArea.style.position = "fixed";
+    textArea.style.left = "-999999px";
+    textArea.style.top = "-999999px";
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    document.execCommand('copy');
+    return textArea.remove();
+  };
+
+  const copyToClipboard = textToCopy => {
+    if (navigator.clipboard && window.isSecureContext) {
+      return navigator.clipboard.writeText(textToCopy);
+    }
+    return fallback(textToCopy);
+  };
+
+
   const handleClick = () => {
     const text = paragraphs.join('\n');
-    navigator.clipboard.writeText(text);
+    copyToClipboard(text);
   };
 
   if (paragraphs.length < 1) {
